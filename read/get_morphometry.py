1ea02466b891882217be8aed7cc743578fe3fbb6
zmiller22 <zm93950@gmail.com>
2020-03-30 18:14:00 -0400

    saved

--- read/get_morphometry.py
+++ read/get_morphometry.py
@@ -12,19 +12,18 @@ import numpy as np
 import pandas as pd
 
 import os
-import sys
 import argparse
 
-import tkinter
-from tkinter import filedialog
 
 import helper_functions as my_func
 
 class NeuronGroup:
     def __init__(self):
-        self.nrn_df_list = []
+        self.nrn_list = []
+        self.morphometry_data = None
         
-    def loadNeurons(self, dir_path):
+    def loadNeurons(self, dir_path, nrn_names=None):
+    #TODO give option to specify neuron names
     #TODO document
     
         filenames = my_func.getFilenames(dir_path)
@@ -33,43 +32,34 @@ class NeuronGroup:
             file_path = dir_path + '/' + filename
             nrn_df = pd.read_csv(file_path, header=None, comment='#',
                                  delim_whitespace=True)
-            self.nrn_df_list.append(nrn_df)
+            self.nrn_list.append((nrn_df, os.path.splitext(filename)[0]))
             
-    def loadNeuron(self, file_path):
+    def loadNeuron(self, file_path, nrn_name=None):
+        #TODO give option to specify neuron name
         #TODO document
-        
+        nrn_name = os.path.splitext(os.path.basename(file_path))[0]
         nrn_df = pd.read_csv(file_path, header=None, comment="#",
                          delim_whitespace=True)
-        self.nrn_df_list.append(nrn_df)
+        self.nrn_list.append((nrn_df, nrn_name))
+        
+    def loadExternalMorphometry():
+        #TODO write a function that allows users to either set the morphometry
+        # data to be an external dataframe (if morphometry_data==None) or 
+        # combine the external morphometry data with the current morphometry
+        # data by row name
+        pass
+    
+    def getMorphometryData():
+        #TODO This should be a command that gets all of the morphometry
+        # data for this NeuronGroup and returns a nice dataframe or combines
+        # it by row name with an existing morphometry dataframe
+        pass
 
         
-
-def loadNeuron(file_path):
-    #TODO document
     
-    # Read in the swc file as a dataframe
-    nrn_df = pd.read_csv(file_path, header=None, comment="#",
-                         delim_whitespace=True)
-    
-    return nrn_df
-
-def loadNeurons(dir_path):
-    #TODO document
-    
-    filenames = my_func.getFilenames(dir_path)
-    nrn_df_list = []
-    
-    for filename in filenames:
-        file_path = dir_path + '/' + filename
-        nrn_df = pd.read_csv(file_path, header=None, comment='#',
-                             delim_whitespace=True)
-        nrn_df_list.append(nrn_df)
-        
-    return nrn_df_list
-        
-    
-def getSomaLoc(nrn_df):
-    """Returns the average (x,y,z) coordinate for the soma of a swc file
+def getSomaPoint(nrn_df):
+    """Returns the average (x,y,z) coordinate for the soma of a neuron swc file
+    dataframe
     
     Args:
         file_path (str): path to the swc file
@@ -85,28 +75,20 @@ def getSomaLoc(nrn_df):
     
     return soma_point
 
-def getNeuriteTerminationPoints(file_path):
-    return None
-
-def choosePath():
-    #TODO comment and document
-    root = tkinter.Tk()
-    root.withdraw()
+def getNeuriteTerminationPoints(nrn_df):
+    #TODO document
+    values = nrn_df.values
+    sample_nums = values[:,0]
+    parent_nums = values[:,6]
     
-    currdir = os.getcwd()
-    chosendir = filedialog.askdirectory(parent=root, initialdir=currdir, title='Please select a directory')
-    if len(chosendir) > 0:
-        print ("Selected Path: %s" % chosendir)
-        
-    return chosendir
+    end_point_rows = np.setdiff1d(sample_nums, parent_nums)
+    end_point_idxs = np.nonzero(np.in1d(sample_nums, end_point_rows))[0]
+    end_points = values[end_point_idxs, 2:5]
+    
+    return end_points
 
-def loadNeuronMorphometry():
-    return None
 
-def loadNeuronsMorphometry():
-    return None
-
-#%%
+#%% Functionality when run as a script
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('-d', '--neuron_directory', required=False,
@@ -119,13 +101,13 @@ if __name__ == '__main__':
     args = vars(ap.parse_args())
     
     if args['neuron_directory'] == None:
-        args['neuron_directory'] = choosePath()
+        args['neuron_directory'] = my_func.choosePath()
         
     if args['config_file'] == None:
-        args['config_file'] = choosePath()
+        args['config_file'] = my_func.choosePath()
         
     if args['output_file'] == None:
-        args['output_file'] = choosePath()
+        args['output_file'] = my_func.choosePath()
         
         
         
